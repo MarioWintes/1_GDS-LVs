911d9164ddef1e4fc21d9c44e96c7aa7484235c0
MarioWintes <52406123@edu.campus02.at>
2024-11-16 16:06:31 +0100

    Lv4

---
+++ src/lv4_oop/Student.java
@@ -0,0 +1,37 @@
+package lv4_oop;
+
+import java.util.Objects;
+
+public class Student {
+
+    private int matrNr;
+    private String fullName;
+
+    //Alt + EINF oder Code->generate->construktor
+    public Student(int matrNr, String fullName) {
+        this.matrNr = matrNr;
+        this.fullName = fullName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return matrNr == student.matrNr;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(matrNr);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "matrNr=" + matrNr +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+}
